ab9fb76b190c48a1f48449736d2b5cb566e8f38b
boboPrem1 <boboprem1@gmail.com>
2023-06-24 21:06:41 +0000

    Created I have just learned the basics of math, comments, semi colon writing at the end of a line, void function writing, and basic of importing dart module

---
+++ 03-basic-math.dart
@@ -0,0 +1,33 @@
+/*
+  In this file we are going to learn about
+
+  main() call
+
+  print() and semicolons at end of lines
+
+  Basic Arithmetics
+
+  Comments
+    Single line comment
+    Multi-line comment
+
+  Importing packages 
+*/
+
+import "dart:math";
+
+void main() {
+  print("hello");
+  print(1+1); // Single line comment Addition
+  print(10-2); // Substraction
+  print(3*3); // Multiplication
+  print(4/2); // Dividion
+  print(10/0);  //Division by zero 
+  print(0/10);  //Zero division
+  print(pow(2,4)); //Pow function from math librairie to get power for a given termes
+  /*
+   * This is multi line comments, the second line
+   * The third line
+   * And so on ...
+  */
+}
