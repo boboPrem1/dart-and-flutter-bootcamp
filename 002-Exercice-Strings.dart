172d7fb483d4915deaf11c7c700e7099ef544709
boboPrem1 <boboprem1@gmail.com>
2023-07-01 20:55:51 +0000

    Exercice one about strings resolved

---
+++ 002-Exercice-Strings.dart
@@ -0,0 +1,35 @@
+void main() {
+// Numbers exercices
+// Given the double below, assign it to a new variable
+// where its rounded to the nearest integer.
+
+var someNum = 3.1;
+ 
+// Bonus question; In case of 0.5 how can you
+//   ensure its rounded up or down
+  
+
+  
+// Responses
+ 
+var roundedSomeNum = someNum.round();
+  
+print(roundedSomeNum);
+  
+// Bonus question responses
+  
+// If we use the ceil method in place of the round methods
+// We are sure to get the rounded up value of someNum
+  
+// And if whe use floor in place of ceil or round we are sure to get
+// The rounded down value of someNum
+
+// round() return a integer rounded form of the numbers or double
+// ceil() return a integer rounded up form of the numbers or double
+// floor() return a integer rounded down form of the numbers or double
+
+// roundToDouble() return a double type rounded form of the numbers or double
+// ceilToDouble() return a double type rounded up form of the numbers or double
+// floorToDouble() return a double type rounded down form of the numbers or double
+
+}
