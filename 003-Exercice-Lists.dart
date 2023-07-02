9b96e3276c7d66b548ad86a3cf6ea78af20d2a5e
boboPrem1 <boboprem1@gmail.com>
2023-07-02 18:33:44 +0000

    Exercice lists resolved

---
+++ 003-Exercice-Lists.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Show two différents ways of grabbing the last element of a list
+
+  var myList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  // First method
+  print(myList[myList.length - 1]);
+
+  // Second method
+  print(myList.last);
+}
