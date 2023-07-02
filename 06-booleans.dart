b86a85592a975cdf739ff4921e939db093ec0af0
boboPrem1 <boboprem1@gmail.com>
2023-07-02 14:04:39 +0000

    Learnings booléans

---
+++ 06-booleans.dart
@@ -0,0 +1,8 @@
+void main() {
+  var theTruth = true;
+  var falseHood = false;
+  print(theTruth);
+  // print true to the console
+  print(falseHood.runtimeType);
+  // print bool to the console
+}
