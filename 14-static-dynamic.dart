918159b162d93171021fba023af98b9aca6938e0
boboPrem1 <boboprem1@gmail.com>
2023-07-03 01:02:55 +0000

    static and dynamic typing in dart

---
+++ 14-static-dynamic.dart
@@ -0,0 +1,15 @@
+void main() {
+// Static typing
+
+var a = 10; // int variable
+
+// a = "Alohaaa"; // error: A value of type 'String' can't be assigned to a variable of type 'int'.
+
+// Dynamic typing
+
+dynamic b = 10; // dynamic variable
+
+b = "Alohaaa"; // no error
+
+print(b); // print Alohaaa
+}
