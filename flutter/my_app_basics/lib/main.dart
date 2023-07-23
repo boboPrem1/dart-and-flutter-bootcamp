4ed35d1477e573948fa89981146592a9e1761bf1
boboPrem1 <boboprem1@gmail.com>
2023-07-23 21:44:00 +0000

    Done basic gesture with a button stateless widget and a gesture detector to detect whend the user tap on the button

--- flutter/my_app_basics/lib/main.dart
+++ flutter/my_app_basics/lib/main.dart
@@ -1,22 +1,73 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(MaterialApp(
+      home: Scaffold(
+          body: Center(
+    child: UserButton(),
+  ))));
 }
 
-class MyApp extends StatelessWidget {
+class UserButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello world new!"),
-      ),
-      body: Center(
-        child: Text("The first flutter app I,ve made!"),
+    return GestureDetector(
+      onTap: () {
+        // On tap child widget inside of gesture detector, what should happens
+        print('The user button was tapped!');
+      },
+      child: Container(
+        height: 60.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.red[500],
+        ),
+        child: const Center(child: (Text("Tap on this button!"))),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void main() {
+//   runApp(MaterialApp(home: MyApp()));
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Hello world new!"),
+//       ),
+//       body: Center(
+//         child: Text("The first flutter app I,ve made!"),
+//       ),
+//     );
+//   }
+// }
 
 // void main() {
 //   runApp(MaterialApp(
